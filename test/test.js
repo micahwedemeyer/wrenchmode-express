7f3a0ed552dea83a6012ab3c5ad3327eeb45b2fd
Micah Wedemeyer <me@micahwedemeyer.com>
2016-05-30 08:41:05 -0400

    First cut of basic tests

---
+++ test/test.js
@@ -0,0 +1,10 @@
+var assert = require('chai').assert;
+
+
+describe("WrenchmodeExpress", function() {
+  describe("#getStatus", function() {
+    it("should be true", function() {
+      assert(true);
+    });
+  });
+});
