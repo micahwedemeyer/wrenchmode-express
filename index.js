10d4770026d4b034a58fddd41110340e3918fa24
Micah Wedemeyer <me@micahwedemeyer.com>
2016-05-30 15:31:41 -0400

    First skeleton of status request

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var http = require('http');
+var https = require('https');
+
 var opts = {
   forceOpen: false,
   ignoreTestMode: true,
@@ -39,7 +42,49 @@ function statusCheck(currentStatus) {
 
   // Update the local status
   currentStatus.switched = true;
-  currentStatus.switchURL = "http://example.com";
+  currentStatus.switchUrl = "http://example.com";
+}
+
+function retrieveStatus(currentStatus) {
+  var options = {
+    hostname: opts.statusHost,
+    path: opts.statusPath,
+    method: 'POST'
+  };
+
+  // TODO: Set the JWT in the header
+
+  var protocol = statusProtocol === 'http' ? http : https;
+  var req = protocol.request(options, (res) => {
+    // TODO: Check status code
+
+    var responseBody = '';
+    res.on('data', (d) => {
+      responseBody += d;
+    });
+
+    res.on('end', () => {
+      // TODO: Catch JSON parse error
+      updateStatus(currentStatus, JSON.parse(responseBody));
+    });
+  });
+
+  // TODO: Write the basic status info to the server in the POST body
+
+  req.end();
+
+  req.on('error', (e) => {
+    currentStatus.switched = false;
+  });
+}
+
+function updateStatus(currentStatus, serverResponse) {
+  if(serverResponse.is_switched) {
+    currentStatus.switched = true;
+    currentStatus.switchUrl = serverResponse.switch_url;
+  } else {
+    currentStatus.switched = false;
+  }
 }
 
 module.exports = wrenchmodeExpress;
