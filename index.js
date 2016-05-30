b72d777da7095cdd98014fe4ee77dd5a9b2f54c1
Micah Wedemeyer <me@micahwedemeyer.com>
2016-05-30 14:24:18 -0400

    Basic function

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
+var opts = {
+  forceOpen: false,
+  ignoreTestMode: true,
+  disableLocalWrench: false,
+  statusProtocol: "https",
+  statusHost: "wrenchmode.com",
+  statusPath: "/api/projects/status",
+  checkDelaySecs: 5,
+  logging: false,
+  readTimeoutSecs: 3
+};
+
+var jwt;
+var statusUrl;
+
 function wrenchmodeExpress(options) {
-  return function(req, res, next) {
-    next();
+  Object.assign(opts, options);
+
+  jwt = opts.jwt;
+  statusUrl = opts.statusProtocol + "://" + opts.statusHost + opts.statusPath;
+
+  // Set up the periodic status checking
+  var currentStatus = {
+    switched: false
   };
+  setInterval(statusCheck.bind(this,currentStatus), opts.checkDelaySecs * 1000);
+
+  return function(req, res, next) {
+    if( !currentStatus.switched ) {
+      next();
+    } else {
+      res.redirect(302, currentStatus.switchURL);
+    }
+  };
+}
+
+function statusCheck(currentStatus) {
+  // Retrieve status from the WM server
+
+  // Update the local status
+  currentStatus.switched = true;
+  currentStatus.switchURL = "http://example.com";
 }
 
 module.exports = wrenchmodeExpress;
--- test/test.js
+++ test/test.js
@@ -17,11 +17,35 @@ describe("WrenchmodeExpress", function() {
       response = httpMocks.createResponse();
     });
 
-
     it("should call the next middleware in the chain", function(done) {
-      let middleware = wrenchmodeExpress({})(request, response, function() {
+      wrenchmodeExpress({})(request, response, function() {
         done();
       });
+    });
+  });
+
+  describe("in wrenchmode", function() {
+    beforeEach(function() {
+      request = httpMocks.createRequest({
+        method: 'GET',
+        url: '/myapp/somepath'
+      });
+      response = httpMocks.createResponse();
+    });
+
+    it("should redirect to Wrenchmode", function(done) {
+      let options = {
+        checkDelaySecs: 0.001
+      };
+
+      let middleware = wrenchmodeExpress(options);
+
+      // Give it a little time to get a response from our fake Wrenchmode server
+      setTimeout(function() {
+        middleware(request, response, function() {})
+        assert.equal(302, response.statusCode);
+        done();
+      }, 10);
     });
   });
 });
